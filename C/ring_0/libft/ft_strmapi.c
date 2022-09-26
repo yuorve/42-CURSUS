0e8b2e9ec2f7c3944bfdf366eee2dde28eb64c0b
Yury Oropeza <yuryoropeza@hotmail.com>
2022-09-26 11:43:35 +0000

    09-26

--- C/ring_0/libft/ft_strmapi.c
+++ C/ring_0/libft/ft_strmapi.c
@@ -1,53 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/26 17:31:21 by yoropeza          #+#    #+#             */
-/*   Updated: 2022/09/26 18:19:20 by yoropeza         ###   ########.fr       */
+/*   Created: 2022/09/26 19:31:21 by yoropeza          #+#    #+#             */
+/*   Updated: 2022/09/26 20:19:20 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_nblen(int n)
-{
-	size_t	len;
-
-	len = 0;
-	if (n < 0)
-		len++;
-	while (n != 0)
-	{
-		n /= 10;
-		len++;
-	}
-	return (len);
-}
-
-char	*ft_itoa(int n)
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*tab;
-	int		len;
-	int		i;
+	size_t	i;
+	size_t	len;
 
-	i = 0;
-	len = ft_nblen(n);
+	len =  ft_strlen(s);
 	tab = (char *) malloc (sizeof(char) * (len + 1));
 	if (!tab)
 		return (0);
-	tab[len--] = '\0';
-	if (n < 0)
+	i = 0;
+	while (s[i])
 	{
-		tab[0] = '-';
-		n *= -1;		
+		tab[i] = f(i, s[i]);
+		i++;
 	}
-	while (n != 0)
-	{
-		tab[len--] = n % 10 + '0';
-		n /= 10;		
-	}
-	return(tab);
+	tab[i] = '\0';
+	return (tab);
 }
--- C/ring_0/libft/libft.h
+++ C/ring_0/libft/libft.h
@@ -41,6 +41,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
