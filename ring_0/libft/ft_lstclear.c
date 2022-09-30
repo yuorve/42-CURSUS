2ca9f8fe8921ff86346e445116abb419c81b63b1
Yury Oropeza <yuorve@users.noreply.github.com>
2022-09-30 20:20:50 +0200

    Add files via upload

---
+++ ring_0/libft/ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 09:57:37 by yoropeza          #+#    #+#             */
+/*   Updated: 2022/09/28 19:32:54 by yoropeza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*node;
+
+	if (!*lst)
+		return ;
+	while (*lst)
+	{
+		node = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = node;
+	}		
+	free(node);
+}
