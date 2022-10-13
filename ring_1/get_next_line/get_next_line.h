cd14169688e2935ce3f943c606cd515c210c59d8
Yury Leonardo Oropeza Veracierto <yoropeza@c3r5s3.42malaga.com>
2022-10-13 19:20:48 +0200

    .

---
+++ ring_1/get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 18:56:45 by yoropeza          #+#    #+#             */
+/*   Updated: 2022/10/13 19:06:38 by yoropeza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
