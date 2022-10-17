6d65772e37c29b021c0977903460e5eb92ce47f5
Yury Oropeza <yuryoropeza@hotmail.com>
2022-10-17 11:38:02 +0000

    get-next-line

--- ring_1/get_next_line/get_next_line.c
+++ ring_1/get_next_line/get_next_line.c
@@ -6,11 +6,24 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 18:58:24 by yoropeza          #+#    #+#             */
-/*   Updated: 2022/10/13 19:22:06 by yoropeza         ###   ########.fr       */
+/*   Updated: 2022/10/17 10:24:27 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
+	char		*line;
+	int			numbytes;
+	
+	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	numbytes = read_file(fd, buffer, BUFFER_SIZE);
+	if (numbytes < 0)
+		return (NULL);
+	if (!ft_strchr(buffer, '\n'))
+		line = join(line, buffer);
 }
