bdc450702ad6f0b8875c9f766f6086892d02ea61
Yury Oropeza <yuryoropeza@hotmail.com>
2022-10-27 16:30:57 +0000

    get-next-line-bonus

---
+++ ring_1/get_next_line/get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 18:56:45 by yoropeza          #+#    #+#             */
+/*   Updated: 2022/10/27 15:56:36 by yoropeza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+size_t	ft_strlen(const char *str);
+
+char	*ft_strdup(char *src);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char    *ft_strjoin(char const *s1, char const *s2);
+char	*get_next_line(int fd);
+
+#endif
