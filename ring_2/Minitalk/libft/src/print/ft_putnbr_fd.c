5df1f02555e421c358d57f7b4e1378c5aeb4261e
Yury Oropeza <yuryoropeza@hotmail.com>
2023-05-11 12:09:25 +0000

    Minitalk

--- ring_2/Minitalk/libft/src/print/ft_putnbr_fd.c
+++ ring_2/Minitalk/libft/src/print/ft_putnbr_fd.c
@@ -6,39 +6,34 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 16:39:10 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/05/11 09:54:30 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/05/11 11:55:13 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/libft.h"
 
-static void	ft_putchar(char c, int fd)
+void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
 
-static void	ft_putnbr(int nb, int fd)
+void	ft_putnbr_fd(int nb, int fd)
 {
 	if (nb > -2147483648)
 	{
 		if (nb < 0)
 		{
-			ft_putchar('-', fd);
+			ft_putchar_fd('-', fd);
 			nb *= -1;
 		}
 		if (nb >= 10)
 		{
-			ft_putnbr((nb / 10), fd);
+			ft_putnbr_fd((nb / 10), fd);
 			nb = nb % 10;
 		}
 		if (nb < 10)
-			ft_putchar((nb + '0'), fd);
+			ft_putchar_fd((nb + '0'), fd);
 	}
 	else
 		write(fd, "-2147483648", 11);
-}
-
-void	ft_putnbr_fd(int n, int fd)
-{
-	ft_putnbr(n, fd);
 }
