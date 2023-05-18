b041c6269c5179dde233e9a38e76be3baa68d2fe
Yury Oropeza <yuryoropeza@hotmail.com>
2023-05-18 09:17:55 +0000

    Minitalk - norminette ok

--- ring_2/Minitalk/server.c
+++ ring_2/Minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 08:25:49 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/05/18 07:59:39 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/05/18 09:17:41 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 // Luego verificar que la señal recibida sea SIGUSR1 o SIGUSR2
 // Si es SIGUSR1 añado el bit en 1 sino en 0
 // Incrementar el índice para el siguiente bit
-// Si se han recibido los 8 bits de un caracter, mostrarlo y reiniciar las variables
+// Si se han recibido los 8 bits, mostrarlo
 // Si es el fin del mensaje termino la comunicación
 // Al final enviar una señal de confirmación al cliente
 void	ft_handler(int sig, siginfo_t *info, void *context)
@@ -27,7 +27,7 @@ void	ft_handler(int sig, siginfo_t *info, void *context)
 	static char	c;
 
 	(void)context;
-	pid_client = info->si_pid;		
+	pid_client = info->si_pid;
 	if (sig == SIGUSR1)
 		c = (c << 1) | 1;
 	else if (sig == SIGUSR2)
@@ -35,7 +35,8 @@ void	ft_handler(int sig, siginfo_t *info, void *context)
 	i++;	
 	if (i == 8)
 	{		
-		if (c == '\0') {
+		if (c == '\0')
+		{
 			ft_printf("\n", c);		
 			kill(pid_client, SIGUSR1);
 		}
@@ -49,24 +50,24 @@ void	ft_handler(int sig, siginfo_t *info, void *context)
 // Lo primero es inicializar la estructura del sigaction
 // La  llamada  al  sistema  sigaction  se emplea para cambiar la acción 
 // tomada por un proceso cuando recibe una determinada señal.
-// Hay que configurar la estructura sigaction para manejar la señal SIGUSR1 y SIGUSR2
+// Hay que configurar sigaction para manejar la señal SIGUSR1 y SIGUSR2
 // Mostrar el PID del servidor
 // Esperar el mensaje del cliente
 int	main(void) {
-	struct sigaction    sa;
-	int 				pid_server;
+	struct sigaction	sa;
+	int					pid_server;
 
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = ft_handler;
 	sigemptyset(&sa.sa_mask);
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		return (ft_printf("Error: Fallo al configurar el controlador de señales\n"));	
+		return (ft_printf("Error: Fallo config USR1\n"));	
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-		return (ft_printf("Error: Fallo al configurar el controlador de señales\n"));	
+		return (ft_printf("Error: Fallo config USR2\n"));	
 	pid_server = getpid();
 	ft_printf("Servidor iniciado\n");
 	ft_printf("PID: %d\n", pid_server);
-	ft_printf("Esperando mensajes...\n");	
+	ft_printf("Esperando mensajes...\n");
 	while (1) 
 		pause();
 	return (0);
