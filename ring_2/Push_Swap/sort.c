762003cc3a2da96d981a861a43ac62f66a57a9bc
Yury Oropeza <yuryoropeza@hotmail.com>
2023-05-18 16:23:23 +0000

    Push_swap - norminette ok

--- ring_2/Push_Swap/sort.c
+++ ring_2/Push_Swap/sort.c
@@ -6,7 +6,7 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 09:49:55 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/05/18 16:13:42 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/05/18 16:23:09 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int	sorting(int *stack_a, int *stack_b, int *stack_tmp, int size)
 
 	i = 0;
 	j = 0;
-	while (size > 0 && !is_sorted(stack_a, size))
+	while (!is_sorted(stack_a, size))
 	{		
 		if (size <= 3)
 			sort_small(stack_a, size);
