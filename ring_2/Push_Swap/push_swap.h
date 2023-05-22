5e7417295cd4439ca92bff0ce1e6dfc9e0f5fbc9
Yury Oropeza <yuryoropeza@hotmail.com>
2023-05-22 10:25:09 +0000

    Push_swap - norminette ok

--- ring_2/Push_Swap/push_swap.h
+++ ring_2/Push_Swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:45:25 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/05/22 08:49:01 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/05/22 10:20:46 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,16 @@
 # define PUSH_SWAP_H
 # include "./libft/inc/libft.h"
 
-typedef struct stacks
-{
-	int *stack_a;
-	int *stack_b;
-    int *stack_tmp;
-	int len_a;
-	int len_b;
-    int len_tmp;
-	int iter_a;
-	int iter_b;
-	int iter_tmp;
-}		t_stacks;
-
 int stack_size(int *stack);
 int has_dupe(int *stack, int n);
 
 void push(int *stack_a, int *stack_b, char c, int size);
 void swap(int *stack, char c);
 void rotate(int *stack, int size, char c);
+void rev_rotate(int *stack, int size, char c);
 void sort_small(int *stack_a, int size);
+void sort_small_2(int *stack_a, int size);
+void sort_small_3(int *stack_a, int size);
 void sort_large(int *stack_a, int *stack_b, int size);
 void count_sort(int *stack_a, int n, int exp, int *output);
 void radix_sort(int *stack_a, int n);
