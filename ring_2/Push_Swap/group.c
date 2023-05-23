0eba19f03c0167ea088371f86f051a55f5b8cde8
Yury Oropeza <yuryoropeza@hotmail.com>
2023-05-23 11:14:20 +0000

    Push_swap - improvements

--- ring_2/Push_Swap/group.c
+++ ring_2/Push_Swap/group.c
@@ -6,7 +6,7 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 08:20:42 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/05/23 11:09:42 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/05/23 11:13:59 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	pre_sort(int *stack_a, int *stack_b, int size)
 	int	len_a;
 	int	len_b;
 
-	j = 0;	
+	j = 0;
 	while (++j < 4)
 	{
 		len_a = size;
@@ -42,14 +42,14 @@ void	pre_sort(int *stack_a, int *stack_b, int size)
 		while (++i < len_a)
 		{
 			if (stack_a[i] >= ((size / 3) * (j - 1))
-			&& stack_a[i] <= ((size / 3) * j))
+				&& stack_a[i] <= ((size / 3) * j))
 			{
 				push(stack_a, stack_b, 'b', len_a);
 				len_a--;
 				len_b++;
 				i--;
 			}
-			else 
+			else
 				rotate(stack_a, len_a, 'a');
 		}
 		come_back(stack_a, stack_b, len_b);
