0d160c5875f89701da982d6efd59bdfb22d3842b
Yury Oropeza <yuryoropeza@hotmail.com>
2023-05-23 11:28:17 +0000

    Push_swap - improvements

--- ring_2/Push_Swap/group.c
+++ ring_2/Push_Swap/group.c
@@ -6,13 +6,13 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 08:20:42 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/05/23 11:13:59 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/05/23 11:28:00 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// Función para devolver a la pila original
+// Función para devolver el tercio a la pila original
 void	come_back(int *stack_a, int *stack_b, int size)
 {
 	int	i;
@@ -25,33 +25,40 @@ void	come_back(int *stack_a, int *stack_b, int size)
 	}
 }
 
-// Función para pre-ordenar pilas grandes ( > 25)
-void	pre_sort(int *stack_a, int *stack_b, int size)
+// Función para pre-ordenar por tercios
+void	split_stack(int *stack_a, int *stack_b, int size, int j)
 {
 	int	i;
-	int	j;
 	int	len_a;
 	int	len_b;
+
+	len_a = size;
+	len_b = 0;
+	i = -1;
+	while (++i < len_a)
+	{
+		if (stack_a[i] >= ((size / 3) * (j - 1))
+			&& stack_a[i] <= ((size / 3) * j))
+		{
+			push(stack_a, stack_b, 'b', len_a);
+			len_a--;
+			len_b++;
+			i--;
+		}
+		else
+			rotate(stack_a, len_a, 'a');
+	}
+	come_back(stack_a, stack_b, len_b);	
+}
+
+// Función para divir en tercios las pilas grandes ( > 25)
+void	pre_sort(int *stack_a, int *stack_b, int size)
+{
+	int	j;
 
 	j = 0;
 	while (++j < 4)
 	{
-		len_a = size;
-		len_b = 0;
-		i = -1;
-		while (++i < len_a)
-		{
-			if (stack_a[i] >= ((size / 3) * (j - 1))
-				&& stack_a[i] <= ((size / 3) * j))
-			{
-				push(stack_a, stack_b, 'b', len_a);
-				len_a--;
-				len_b++;
-				i--;
-			}
-			else
-				rotate(stack_a, len_a, 'a');
-		}
-		come_back(stack_a, stack_b, len_b);
+		split_stack(stack_a, stack_b, size, j);
 	}
 }
--- ring_2/Push_Swap/push_swap.h
+++ ring_2/Push_Swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:45:25 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/05/23 09:22:40 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/05/23 11:26:23 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void	sort_large(int *stack_a, int *stack_b, int size);
 void	count_sort(int *stack_a, int n, int exp, int *output);
 void	radix_sort(int *stack_a, int n);
 void	come_back(int *stack_a, int *stack_b, int size);
+void    split_stack(int *stack_a, int *stack_b, int size, int j);
 void	pre_sort(int *stack_a, int *stack_b, int size);
 void	router(int *stack_a, int *stack_b, int size);
 void	direction(int *stack, int n, int size);
