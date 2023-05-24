7b12b843790092f852dc5661edd1dd083233b515
Yury Oropeza <yuryoropeza@hotmail.com>
2023-05-24 07:05:17 +0000

    Push_swap - improvements

--- ring_2/Push_Swap/group.c
+++ ring_2/Push_Swap/group.c
@@ -6,7 +6,7 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 08:20:42 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/05/23 20:45:45 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/05/24 06:59:26 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ void	come_back(int *stack_a, int *stack_b, int size)
 
 	i = 0;
 	while (i < size)
-	{
+	{		
 		push(stack_a, stack_b, 'a', stack_size(stack_a) + 1);
+		if (stack_a[1] < stack_a[0])
+			swap(stack_a, 'a');
 		i++;
 	}
 }
@@ -31,7 +33,7 @@ void	split_stack(int *stack_a, int *stack_b, int min, int max)
 	int	i;
 	int	size;
 
-	size = stack_size(stack_a);	
+	size = stack_size(stack_a);
 	i = -1;
 	while (++i < size)
 	{
@@ -50,15 +52,15 @@ void	split_stack(int *stack_a, int *stack_b, int min, int max)
 void	pre_sort(int *stack_a, int *stack_b, int size)
 {
 	int	j;
-	int max;
-	int min;
+	int	max;
+	int	min;
 
 	j = 0;
 	while (++j < 4)
 	{
 		min = ((size / 3) * (j - 1));
 		max = ((size / 3) * j);
-		split_stack(stack_a, stack_b, min, max);		
+		split_stack(stack_a, stack_b, min, max);
 	}
 	come_back(stack_a, stack_b, stack_size(stack_b));
 }
