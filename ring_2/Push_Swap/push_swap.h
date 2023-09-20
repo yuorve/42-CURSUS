5355732fd1d0ede728970bfb4a9e4fb30c5ca473
Yury Oropeza <yuorve@users.noreply.github.com>
2023-09-20 17:59:13 +0200

    Push_swap - norminette ok

--- ring_2/Push_Swap/push_swap.h
+++ ring_2/Push_Swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:45:25 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/09/14 08:27:50 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/09/12 16:44:08 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,14 +40,13 @@ typedef struct	s_Data {
 }				t_Data;
 
 //Funciones
-t_Stack		*createStack();
+t_Stack		*create_stack();
 
-void 		freeStack(t_Stack* stack);
+void 		free_stack(t_Stack* stack);
 void 		push(t_Stack* stack, int data);
 void 		check_arguments(t_Data *data, char **argv, int argc);
-void 		sortStack(t_Data *data);
-void 		printStack(t_Data *data);
-void		calcutateMoves(t_Data *data);
+void 		sort_stack(t_Data *data);
+void		calcutate_moves(t_Data *data);
 void		sort_small(t_Data *data);
 void 		sort(t_Data *data);
 void 		pa(t_Data *data);
@@ -62,17 +61,17 @@ void 		rra(t_Data *data);
 void 		rrb(t_Data *data);
 void 		rrr(t_Data *data);
 
-int			isEmpty(t_Stack* stack);
+int			is_empty(t_Stack* stack);
 int			peek(t_Stack* stack);
 int			pook(t_Stack* stack);
 int			pop(t_Stack* stack);
-int 		minStack(t_Node *top);
-int 		maxStack(t_Node *top);
-int 		findPos(t_Node * head, int valueToFind);
-int 		findNext(t_Data *data, int valueToFind);
-int			isSortedStack(t_Data *data);
-int			Moves(t_Data *data, int valueToFind, int controller);
-int			NextMove(t_Data *data, int valueToFind);
+int 		min_stack(t_Node *top);
+int 		max_stack(t_Node *top);
+int 		find_pos(t_Node * head, int valueToFind);
+int 		find_next(t_Data *data, int valueToFind);
+int			is_sorted_stack(t_Data *data);
+int			moves(t_Data *data, int valueToFind, int controller);
+int			next_move(t_Data *data, int valueToFind);
 
 long		ft_atoil(const char *str);
 
