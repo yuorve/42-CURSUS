15de09b8e8d6f5bad2f7461596863daa5fc15413
Yury Oropeza <yuorve@users.noreply.github.com>
2023-11-01 21:13:34 +0100

    Add files via upload

--- ring_2/So_long/inc/so_long.h
+++ ring_2/So_long/inc/so_long.h
@@ -6,7 +6,7 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 11:19:05 by yoropeza          #+#    #+#             */
-/*   Updated: 2023/10/25 17:07:37 by yoropeza         ###   ########.fr       */
+/*   Updated: 2023/11/01 19:53:40 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,39 @@ typedef struct s_point
 {
 	int	x;
 	int	y;
-}   t_point;
+}	t_point;
 
 typedef struct s_data
 {
-	mlx_t	*mlx;
-	char	**map;
-	int		map_width;
-	int		map_heigth;
-	int		keys;
-	int		points;
-}   t_data;
+	mlx_t		*mlx;
+	mlx_image_t	*ground;
+	mlx_image_t *wall;
+	mlx_image_t	*iplayer;
+	mlx_image_t *exit_open;
+	mlx_image_t	*exit_close;
+	mlx_image_t	*ikeys;
+	char		**map;
+	size_t		map_width;
+	size_t		map_height;
+	int			player;
+	int			keys;
+	int			steps;
+	int			points;
+	int			exit;
+	int			isize;
+	t_point		player_pos;
+	t_point		exit_pos;
+}	t_data;
 
+void	quit(t_data *data, char *err);
+void	reading(t_data *data, char *map);
+void	flood_fill(char **matrix, t_point start);
+void	ft_freeplus(char **matrix, int size);
+void	drawing(t_data *data);
+void	my_keyhook(mlx_key_data_t keydata, void *param);
+void	moves(t_data *data, int x, int y);
+
+int		thisistheway(char **matrix, t_point start);
+
+char	**ft_copy(char **matrix, int size);
 #endif
