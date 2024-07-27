97db1c640eb11738da89cdb882bd66bca9009df1
Yury Oropeza <yuryoropeza@hotmail.com>
2024-07-27 17:26:49 +0000

    CPPModule03 ex02

---
+++ ring_4/CPPModule03/ex02/FragTrap.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/27 16:53:03 by yoropeza          #+#    #+#             */
+/*   Updated: 2024/07/27 17:20:52 by yoropeza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ClapTrap() {
+    _hitPoints = 100;
+    _energyPoints = 100;
+    _attackDamage = 30;
+    std::cout << "FragTrap default constructor called" << std::endl;
+}
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name) {
+    _hitPoints = 100;
+    _energyPoints = 100;
+    _attackDamage = 30;
+    std::cout << "FragTrap " << _name << " is created!" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other) {
+    std::cout << "FragTrap copy constructor called" << std::endl;
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &other) {
+    std::cout << "FragTrap assignment operator called" << std::endl;
+    if (this != &other) {
+        ClapTrap::operator=(other);
+    }
+    return *this;
+}
+
+FragTrap::~FragTrap() {
+    std::cout << "FragTrap " << _name << " is destroyed!" << std::endl;
+}
+
+void FragTrap::highFivesGuys() {
+    std::cout << "FragTrap " << _name << ": High five guys!!!" << std::endl;
+}
---
+++ ring_4/CPPModule03/ex02/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/27 16:22:42 by yoropeza          #+#    #+#             */
+/*   Updated: 2024/07/27 17:20:17 by yoropeza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+int main(void) {
+    std::cout << "Creating ClapTrap:" << std::endl;
+    ClapTrap clap("CL4P-TP");
+    
+    std::cout << "\nCreating ScavTrap:" << std::endl;
+    ScavTrap scav("SC4V-TP");
+    
+    std::cout << "\nCreating FragTrap:" << std::endl;
+    FragTrap frag("FR4V-TP");
+    
+    std::cout << "\nTesting ClapTrap:" << std::endl;
+    clap.attack("Bandit");
+    clap.takeDamage(5);
+    clap.beRepaired(3);
+    
+    std::cout << "\nTesting ScavTrap:" << std::endl;
+    scav.attack("Psycho");
+    scav.takeDamage(15);
+    scav.beRepaired(10);
+    scav.guardGate();
+
+    std::cout << "\nTesting FragTrap:" << std::endl;
+    frag.attack("Murder");
+    frag.takeDamage(15);
+    frag.beRepaired(10);
+    frag.highFivesGuys();
+    
+    std::cout << "\nDestroying objects:" << std::endl;
+    
+    return 0;
+}
