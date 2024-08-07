44c02b6e1e8efb0a82c974646b29cb7ef98bbdc4
Yury Oropeza <yuorve@users.noreply.github.com>
2024-08-07 17:44:37 +0200

    CPPModule01 ex01

--- ring_4/CPPModule01/ex01/Zombie.hpp
+++ ring_4/CPPModule01/ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: yoropeza <yoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:52:42 by yoropeza          #+#    #+#             */
-/*   Updated: 2024/05/06 17:51:04 by yoropeza         ###   ########.fr       */
+/*   Updated: 2024/08/07 17:39:36 by yoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,12 @@
 class	Zombie
 {
 	public:
+		Zombie(void);
 		Zombie(std::string name);
 		~Zombie(void);
 		
 		void announce( void );
+		void set_name(std::string name);
 		
 	private:
 		std::string	_name;
